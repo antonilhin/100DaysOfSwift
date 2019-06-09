7ed07568d0b929082b5896bb97924ef6aeeafeca
antonilhin <42436339+antonilhin@users.noreply.github.com>
2019-06-09 18:51:26 +0700

    Project 7 - Whitehouse Petitions
    Part 1: UITabBarController, Data, Codable (JSON Parsing)
    Part 2: LoadHTMLString, didFinishLauncingWithOptions
    Part 3: Wrap up and challanges

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Antoni on 27/5/19.
+//  Copyright © 2019 OneByOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [Petition]()
+    var filteredPetitions: [Petition] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credit", style: .plain, target: self, action: #selector(showCreddit))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetition))
+        
+        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                // we're OK to parse!
+                parse(json: data)
+                return
+            }
+        }
+        showError()
+        
+    }
+    
+    @objc func showCreddit() {
+        let ac = UIAlertController(title: "Credit", message: "These data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterPetition() {
+        let ac = UIAlertController(title: "Filter Petition", message: nil, preferredStyle: .alert)
+
+        ac.addTextField()
+        
+        let submitAction =  UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] action in
+            guard let text = ac?.textFields?[0].text else { return }
+            self?.submit(text)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ text: String) {
+        filteredPetitions.removeAll()
+        for petition in petitions {
+            if petition.title.contains(text) || petition.body.contains(text) {
+                filteredPetitions.append(petition)
+                tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions =  petitions
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Tableview
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = filteredPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filteredPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
