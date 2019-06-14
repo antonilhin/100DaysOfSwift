b69d8642add4fd5c8c40a5526d68d1ec2a81d412
antonilhin <42436339+antonilhin@users.noreply.github.com>
2019-06-14 10:14:17 +0700

    Project 10 - Names to Faces
    Part 1: UICollectionView & UICollectionViewCell
    Part 2: UIImagePickerController, NSObject, Connecting CollectionView with DataSources
    Part 3: Wrap up and challanges

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Antoni on 13/6/19.
+//  Copyright © 2019 OneByOne. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Antoni on 13/6/19.
+//  Copyright © 2019 OneByOne. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
